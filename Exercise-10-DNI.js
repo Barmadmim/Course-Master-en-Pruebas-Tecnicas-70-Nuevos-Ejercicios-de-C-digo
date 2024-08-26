7f6103fcef1be665083685af45e52fe17e601459
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-26 15:21:36 -0500

    Add Exercise-10-DNI

---
+++ Exercise-10-DNI.js
@@ -0,0 +1,25 @@
+/*
+Advertencias:
+- En español por fines didacticos, en la vida real usa nombres en ingles.
+- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
+- Ejercicios genéricos, puedes usar cualquier lenguaje.
+- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
+- Siempre mostrar el resultado en la consola / terminal (salvo excepciones).
+- Hay muchas soluciones validas para un mismo ejercicio.
+ 
+Enunciado Ejercicio 10:
+Crea una función que reciba un DNI (8 números) y calcule la letra del DNI
+ 
+Es un proceso matemático facil que se basa en obtener el 
+resto de la división entera del número de DNI y el número 23. 
+Y con el resto se obtiene la letra, usandolo como posición o indice
+dentro de un array de letras.
+ 
+Este sería el array de letras:
+const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 
+                'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+ 
+Ejemplos:
+generarLetraDNI("25439343");  // Resultado: 25439343D
+ 
+*/
