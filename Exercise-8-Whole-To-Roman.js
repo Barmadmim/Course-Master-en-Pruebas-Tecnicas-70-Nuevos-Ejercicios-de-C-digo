9e3651d863b7e4b16cade5399bc189bae5f7d6c9
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-25 20:38:13 -0500

    Add Exercise-8

---
+++ Exercise-8-Whole-To-Roman.js
@@ -0,0 +1,18 @@
+/*
+Advertencias:
+- En español por fines didacticos, en la vida real usa nombres en ingles.
+- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
+- Ejercicios genéricos, puedes usar cualquier lenguaje.
+- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
+- Siempre mostrar el resultado en la consola / terminal (salvo excepciones).
+- Hay muchas soluciones validas para un mismo ejercicio.
+ 
+Enunciado Ejercicio 8:
+Crea un algoritmo al cual le pase un número entero
+y me lo convierta a número romano
+ 
+Ejemplos:
+enteroARomano(100)); // Resultado:  C
+enteroARomano(345)); // Resultado: CCCXLV
+ 
+*/
