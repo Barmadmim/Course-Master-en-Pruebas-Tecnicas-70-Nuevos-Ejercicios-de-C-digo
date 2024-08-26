9da69531a0cfb610c739d9ca563ed9ddb3e9fbb0
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-25 19:16:18 -0500

    Add Exercise-2

---
+++ Exercise-2-Simplify-Route.js
@@ -0,0 +1,20 @@
+/*
+Advertencias:
+- En español por fines didacticos, en la vida real usa nombres en ingles.
+- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
+- Ejercicios genéricos, puedes usar cualquier lenguaje.
+- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
+- Siempre mostrar el resultado en la consola / terminal.
+- Hay muchas soluciones validas para un mismo ejercicio.
+ 
+Enunciado Ejercicio 2:
+Dada una ruta absoluta de un archivo (de un sistema linux o basado en unix)
+crear una función que la simplifique
+ 
+Ejemplo:
+simplificarRuta("/home/");             // Salida: /home
+simplificarRuta("/x/./y/../../z/");    // Salida: /z
+simplificarRuta("/../");               // Salida: /
+simplificarRuta("/home//pruebas/");    // Salida: /home/pruebas
+ 
+*/
