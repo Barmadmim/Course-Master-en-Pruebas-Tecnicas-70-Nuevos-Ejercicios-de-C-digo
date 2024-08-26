02f7c4241ff29e89396b2fc3cf98c5e1387a979a
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-26 15:28:49 -0500

    Add Exercise-14

---
+++ Exercise-14-Palindromo.js
@@ -0,0 +1,21 @@
+/*
+Advertencias:
+- En español por fines didacticos, en la vida real usa nombres en ingles.
+- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
+- Ejercicios genéricos, puedes usar cualquier lenguaje.
+- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
+- Siempre mostrar el resultado en la consola / terminal (salvo excepciones).
+- Hay muchas soluciones validas para un mismo ejercicio.
+ 
+Enunciado Ejercicio 14:
+Dada una cadena de texto, comprobar si es un palindromo o no. No usar funciones de
+javascript, solo estructuras de control
+ 
+Los palíndromos son palabras que se leen igual aun estando invertidas. 
+Por ejemplo: ana, bob, otto, allivessevilla
+ 
+Ejemplos:
+esPalindromo("otto") // Devuelve: true
+esPalindromo("victor") // Devuelve: false
+ 
+*/
