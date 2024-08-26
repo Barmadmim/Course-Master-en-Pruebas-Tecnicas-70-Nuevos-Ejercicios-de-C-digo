85b483386df4e8241fa62eb5b507d421aa596446
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-26 15:35:08 -0500

    Add Exercise-15

---
+++ Exercise-15-Sort-By-Letter.js
@@ -0,0 +1,30 @@
+/*
+Advertencias:
+- En español por fines didacticos, en la vida real usa nombres en ingles.
+- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
+- Ejercicios genéricos, puedes usar cualquier lenguaje.
+- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
+- Siempre mostrar el resultado en la consola / terminal (salvo excepciones).
+- Hay muchas soluciones validas para un mismo ejercicio.
+ 
+Enunciado Ejercicio 15:
+Dada un array de palabras, crear una función que lo devuelva ordenado
+por la longitud de sus palabras de menor a mayor
+ 
+Ejemplos:
+ordenarPorLongitud([
+  "Hola",
+  "soy",
+  "Víctor Robles",
+  "como",
+  "estas",
+  "hoy",
+  "amigo",
+  "yo",
+  "voy",
+  "a",
+  "estudiar",
+  "ahora"
+]);
+ 
+*/
