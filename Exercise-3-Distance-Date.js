fcaf9e1136813cc33bcb02d54a0ca6fe78623e10
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-25 20:10:01 -0500

    Add Exercise-3

---
+++ Exercise-3-Distance-Date.js
@@ -0,0 +1,17 @@
+/*
+Advertencias:
+- En español por fines didacticos, en la vida real usa nombres en ingles.
+- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
+- Ejercicios genéricos, puedes usar cualquier lenguaje.
+- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
+- Siempre mostrar el resultado en la consola / terminal.
+- Hay muchas soluciones validas para un mismo ejercicio.
+ 
+Enunciado Ejercicio 3:
+Dadas dos fechas, crea una función que me proporcione la diferencia de dias
+entre ellas
+ 
+Ejemplo:
+diferenciaDeDias('Dec 1, 2023', 'Dec 24, 2023');  // Salida: 23
+ 
+*/
