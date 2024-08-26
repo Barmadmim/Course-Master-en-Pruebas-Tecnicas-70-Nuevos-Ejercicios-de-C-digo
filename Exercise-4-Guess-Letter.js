4774d96530e2b786e725b4abfe7b084317bc23f0
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-26 14:26:17 -0500

    Complete Exercise-4

--- Exercise-4-Guess-Letter.js
+++ Exercise-4-Guess-Letter.js
@@ -25,7 +25,74 @@ Te quedan 5 intentos
  
 */
 
-function juegoDelAhorcado(palabra){
-    // console.log("Prueva esjercisio")
-    // 
+function juegoDelAhorcado(palabraSecreta) {
+
+  // Crear variable palbra oculta con guiones bajos
+  let palabraOculta = "-".repeat(palabraSecreta.length);
+
+  // Variable con numero de intentos
+  let intentos = 5;
+
+  // Bucle para pedir letra que se ejecutara si no se ha adivinado y si hay intentos
+  while (intentos > 0 && palabraOculta !== palabraSecreta) {
+    // Pedir letra
+    let letra = prompt("Agrega la letra que crees que tiene la palabra");
+
+    // Comprobar si la letra esta en la palabra para actualizar la variable palabraOculta
+    if (palabraSecreta.includes(letra)) {
+        
+      // Actualizar la variable palabraOculta y meter la letra en pocision correcta
+      for (
+        let contadorLetras = 0;
+        contadorLetras < palabraSecreta.length;
+        contadorLetras++
+      ) {
+        if (letra === palabraSecreta[contadorLetras]) {
+          console.log(contadorLetras, palabraSecreta[contadorLetras]);
+
+          // Convertir la palabra oculta en un array
+
+          let palabraOcultaArray = Array.from(palabraOculta);
+
+          // Sellecionar indice y cambiar _ por la letra sellecionado
+
+          palabraOcultaArray[contadorLetras] = letra;
+
+          // Unir el array en un string de nuevo
+
+          palabraOculta = palabraOcultaArray.join("");
+
+          // Actualizar la variable palabraOculta
+        }
+      }
+    } else {
+      // Si no existe la letra en la palabra restar 1 intento
+
+      intentos--;
+    }
+
+    // Mostrar el estado de la palabraOculta
+    console.log(`La palabra es: ${palabraOculta}.`);
+
+    // Mostra numero de intentos faltantes
+
+    console.log(`Te quedan ${intentos} intentos.`);
+  }
+
+  // Cuando salga del bucle
+  // Si se adivinaron las letra (Victori)
+
+  if (palabraOculta === palabraSecreta) {
+    console.log(`Has ganado!!!, La palabra secreta era: ${palabraSecreta}`);
+  } else {
+    // si intentos = 0 derrota
+
+    console.log(
+      `Lo siento, has perdido, La palabra secreta era: ${palabraSecreta}`
+    );
+  }
 }
+
+const lanzarJuego = document
+  .querySelector("#lanzarJuego")
+  .addEventListener("click", () => juegoDelAhorcado("victor"));
