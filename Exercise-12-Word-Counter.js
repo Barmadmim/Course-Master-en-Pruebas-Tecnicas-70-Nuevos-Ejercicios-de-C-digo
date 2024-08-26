ae2e113d5d7f1bd0e50fb656fdb71daaad65c650
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-26 15:26:14 -0500

    Add Exercise-12

---
+++ Exercise-12-Word-Counter.js
@@ -0,0 +1,33 @@
+/*
+Advertencias:
+- En español por fines didacticos, en la vida real usa nombres en ingles.
+- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
+- Ejercicios genéricos, puedes usar cualquier lenguaje.
+- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
+- Siempre mostrar el resultado en la consola / terminal (salvo excepciones).
+- Hay muchas soluciones validas para un mismo ejercicio.
+ 
+Enunciado Ejercicio 12:
+Crea un programa que cuente las palabras de un texto
+ 
+Ejemplos:
+contarPalabras("El perro de san roque no tiene el rabo 
+porque es un perro muy muy malo");
+ 
+Devuelve:
+{
+  el: 2,
+  perro: 2,
+  de: 1,
+  san: 1,
+  roque: 1,
+  no: 1,
+  tiene: 1,
+  rabo: 1,
+  porque: 1,
+  es: 1,
+  un: 1,
+  muy: 2,
+  malo: 1
+}
+*/
