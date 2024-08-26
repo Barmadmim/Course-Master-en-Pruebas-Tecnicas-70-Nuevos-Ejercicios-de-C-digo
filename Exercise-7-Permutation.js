f966130ffd6cea4d0b9669d5b6cbbfb7ec9cda4f
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-25 20:32:48 -0500

    Add Exercise-7

---
+++ Exercise-7-Permutation.js
@@ -0,0 +1,25 @@
+/*
+Advertencias:
+- En español por fines didacticos, en la vida real usa nombres en ingles.
+- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
+- Ejercicios genéricos, puedes usar cualquier lenguaje.
+- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
+- Siempre mostrar el resultado en la consola / terminal (salvo excepciones).
+- Hay muchas soluciones validas para un mismo ejercicio.
+ 
+Enunciado Ejercicio 7:
+Dada un array de enteros, detectar si esa lista
+de números es una permutación del 1 al ultimo número del array.
+ 
+En este caso una permutación es una secuencia de números
+en orden sin que falte ninguno entre ellos.
+ 
+Devolver el número faltante más grande.
+ 
+El array puede venir desordenado.
+ 
+Ejemplos:
+permutacion([1, 2, 3, 4, 5])   // Devuelve: 5
+permutacion([1, 2, 3, 5]))     // Devuelve: 4
+ 
+*/
