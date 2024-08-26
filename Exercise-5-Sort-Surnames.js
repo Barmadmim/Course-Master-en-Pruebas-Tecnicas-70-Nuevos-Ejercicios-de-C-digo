b998a39ff4e4298293f00493d1089ab564ab66ce
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-25 20:29:34 -0500

    Add Exercise-5

---
+++ Exercise-5-Sort-Surnames.js
@@ -0,0 +1,25 @@
+/*
+Advertencias:
+- En español por fines didacticos, en la vida real usa nombres en ingles.
+- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
+- Ejercicios genéricos, puedes usar cualquier lenguaje.
+- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
+- Siempre mostrar el resultado en la consola / terminal (salvo excepciones).
+- Hay muchas soluciones validas para un mismo ejercicio.
+ 
+Enunciado Ejercicio 5:
+Dado un array de personas con nombres y apellidos
+crear una función para ordenar el array por los apellidos
+en orden alfabético
+ 
+Ejemplo:
+ordenarPorApellidos([
+    "Víctor Robles",
+    "Antonio Alcantara",
+    "Al Pacino",
+    "Robert DeNiro",
+    "Brad Pitt",
+    "Sylvester Stallone"
+]);  
+ 
+*/
