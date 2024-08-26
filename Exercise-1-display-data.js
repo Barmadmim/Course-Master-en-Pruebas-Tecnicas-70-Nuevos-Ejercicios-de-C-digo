baad5594b0532576a6dd978590abaa446568f113
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-25 19:50:32 -0500

    Complete Exercise-1

--- Exercise-1-display-data.js
+++ Exercise-1-display-data.js
@@ -24,99 +24,85 @@ Muestra la información de todos los superheroes
 
 // 1º Crear array de objetos
 const infoSuper = {
-    'Iron Man': {
-        nombreReal: 'Tony Stark',
-        poderes: ['Tecnologia avanzada', 'Movilidad aérea'],
-        equipo: 'Los vengadores'
-    },
-    'Capitán América': {
-        nombreReal: 'Steve Rogers',
-        poderes: ['Fuerza sobrehumana', 'Agilidad y reflejos sobresalientes'],
-        equipo: 'Los vengadores'
-    },
-    'Thor': {
-        nombreReal: 'Thor Odinson',
-        poderes: ['Mjolnir', 'Viento y trueno'],
-        equipo: 'Los vengadores'
-    },
-    'Spider-Man': {
-        nombreReal: 'Peter Parker',
-        poderes: ['Balanceo', 'Telarañas pegajosas', 'Sentido aracnido'],
-        equipo: 'Los vengadores'
-    },
-    'Hulk': {
-        nombreReal: 'Bruce Banner',
-        poderes: ['Fuerza sobrehumana', 'Invulnerabilidad'],
-        equipo: 'Los vengadores'
-    }
+  "Iron Man": {
+    nombreReal: "Tony Stark",
+    poderes: ["Tecnologia avanzada", "Movilidad aérea"],
+    equipo: "Los vengadores",
+  },
+  "Capitán América": {
+    nombreReal: "Steve Rogers",
+    poderes: ["Fuerza sobrehumana", "Agilidad y reflejos sobresalientes"],
+    equipo: "Los vengadores",
+  },
+  "Thor": {
+    nombreReal: "Thor Odinson",
+    poderes: ["Mjolnir", "Viento y trueno"],
+    equipo: "Los vengadores",
+  },
+  "Spider-Man": {
+    nombreReal: "Peter Parker",
+    poderes: ["Balanceo", "Telarañas pegajosas", "Sentido aracnido"],
+    equipo: "Los vengadores",
+  },
+  "Hulk": {
+    nombreReal: "Bruce Banner",
+    poderes: ["Fuerza sobrehumana", "Invulnerabilidad"],
+    equipo: "Los vengadores",
+  },
 };
 
-function mostrarInfoSuperHerore(aka){
-
-    console.log(``);
-    console.log(`################## ${aka} ##################`);
-    // Comprobar si el superheroe existe dentro del objeto
-    if(aka in infoSuper){
-    // Conseguir la info de ese superHeroe
-    const informacion = infoSuper[aka];
-    // Mostrar la info
+function mostrarInfoSuperheroe(nombre) {
+  if (nombre in infoSuper) {
+    const informacion = infoSuper[nombre];
     console.log(`Nombre real: ${informacion.nombreReal}`);
-        // Para dar formato al imprimir del array, usamos un '.join', donde se pone el caracter que se coloca entre los elementos
-    console.log(`Poderes: ${informacion.poderes.join(', ')}`);
+    console.log(`Poderes: ${informacion.poderes.join(", ")}`);
     console.log(`Equipo: ${informacion.equipo}`);
-    console.log(``)
-
-    }else{
-        console.log(`${aka} no existe en la base de datos`);
-        console.log(``);
-    }
-
-}
-    // Opcion 1 de declarar funciones
-mostrarInfoSuperHerore("Spider-Man");
-    // Opcion 2 de declarar funciones
-const mostrarInformacionSuperHeroes = (akas) => {
-
-    // Recorrer todos los nombres
-    akas.forEach(nombre => {
-
-        // Mostrar la info (usar funcion anterior)
-        mostrarInfoSuperHerore(nombre);
-        
-    });
-    
+  } else {
+    console.log(`${nombre} no existe en la base de datos`);
+  }
 }
 
-mostrarInformacionSuperHeroes(["Thor", "Hulk", "Joker", "Spider-Man"]);
+mostrarInfoSuperheroe("Hulk");
 
-/* La terminal devuelve: 
-$ node .\ejercicio01.js
+/*
 
-################## Spider-Man ##################
-Nombre real: Peter Parker
-Poderes: Balanceo, Telarañas pegajosas, Sentido aracnido
-Equipo: Los vengadores
+mostrarInfoSuperheroe("Hulk");
 
-
-################## Thor ##################
-Nombre real: Thor Odinson
-Poderes: Mjolnir, Viento y trueno
-Equipo: Los vengadores
-
-
-################## Hulk ##################
 Nombre real: Bruce Banner
 Poderes: Fuerza sobrehumana, Invulnerabilidad
 Equipo: Los vengadores
 
+*/
+    // Opcion 2 de declarar funciones
+    const mostrarInformacionSuperHeroes = (akas) => {
 
-################## Joker ##################
-Joker no existe en la base de datos
+        // Recorrer todos los nombres
+        akas.forEach(nombre => {
+    
+            // Mostrar la info (usar funcion anterior)
+            mostrarInfoSuperHeroe(nombre);
+            
+        });
+        
+    }
+    
+    mostrarInformacionSuperHeroes(["Thor", "Hulk", "Joker", "Spider-Man"]);
 
 
-################## Spider-Man ##################
-Nombre real: Peter Parker
-Poderes: Balanceo, Telarañas pegajosas, Sentido aracnido
-Equipo: Los vengadores
+
+/*
+function mostrarInfoSuperheroes(nombres) {
+    if (nombres in infoSuper) {
+      const informacion = infoSuper[nombres];
+      console.log(`Nombre real: ${informacion.nombreReal}`);
+      console.log(`Poderes: ${informacion.poderes.join(", ")}`);
+      console.log(`Equipo: ${informacion.equipo}`);
+    } else {
+      console.log(`${nombres} no existe en la base de datos`);
+    }
+  }
+  
+  mostrarInfoSuperheroes(["Thor", "Hulk", "Joker", "Spider-Man"]);
+
 
 */
