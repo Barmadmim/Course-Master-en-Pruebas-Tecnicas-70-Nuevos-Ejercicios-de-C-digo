2d117e981da951635cd823168213b0c3324bf4f9
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-25 20:28:36 -0500

    Add Exercise-4

---
+++ Exercise-4-Guess-Letter.js
@@ -0,0 +1,26 @@
+/*
+Advertencias:
+- En español por fines didacticos, en la vida real usa nombres en ingles.
+- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
+- Ejercicios genéricos, puedes usar cualquier lenguaje.
+- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
+- Siempre mostrar el resultado en la consola / terminal (salvo excepciones).
+- Hay muchas soluciones validas para un mismo ejercicio.
+ 
+Enunciado Ejercicio 4:
+Crea el juego del ahorcado.
+El usuario tendrá que ir adivinando y descubriendo las letras de una palabra 
+secreta, y tendrá 5 intentos (un intento por extremidad del cuerpo humano).
+ 
+Utiliza el método de entrada de datos habitual de tu lenguaje,
+en el caso de JS, usaremos la función prompt.
+ 
+Ejemplo:
+juegoDelAhorcado('victor');  
+ 
+// Salida:
+Adivina la letra: i
+La palabra es: _i___
+Te quedan 5 intentos
+ 
+*/
