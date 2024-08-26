8dfc311d8d3fb47d1a951af7c4845472157602c3
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-26 15:27:28 -0500

    Add Exercise-13

---
+++ Exercise-13-Calculate-Area.js
@@ -0,0 +1,18 @@
+/*
+Advertencias:
+- En español por fines didacticos, en la vida real usa nombres en ingles.
+- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
+- Ejercicios genéricos, puedes usar cualquier lenguaje.
+- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
+- Siempre mostrar el resultado en la consola / terminal (salvo excepciones).
+- Hay muchas soluciones validas para un mismo ejercicio.
+ 
+Enunciado Ejercicio 13:
+Crea una funcion que calcule el area de un cuadrado, un rectangulo o un triangulo
+ 
+Ejemplos:
+calcularAreaPoligono({ tipo: 'triangulo', base: 6, altura: 9 }); 
+ 
+Devuelve: 27
+ 
+*/
