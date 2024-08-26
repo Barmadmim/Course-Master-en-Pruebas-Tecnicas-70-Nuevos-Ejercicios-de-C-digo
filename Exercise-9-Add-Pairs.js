6ae7cf8d34496eade9978e7b78898a1f738a9478
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-26 15:20:25 -0500

    Add Exercise-9-Add-Pairs

---
+++ Exercise-9-Add-Pairs.js
@@ -0,0 +1,17 @@
+/*
+Advertencias:
+- En español por fines didacticos, en la vida real usa nombres en ingles.
+- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
+- Ejercicios genéricos, puedes usar cualquier lenguaje.
+- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
+- Siempre mostrar el resultado en la consola / terminal (salvo excepciones).
+- Hay muchas soluciones validas para un mismo ejercicio.
+ 
+Enunciado Ejercicio 9:
+Crea una función que reciba un array de números y calcule
+la suma de todos los números pares del array
+ 
+Ejemplos:
+sumarPares([2, 13, 6, 15]);  // Resultado: 8
+ 
+*/
