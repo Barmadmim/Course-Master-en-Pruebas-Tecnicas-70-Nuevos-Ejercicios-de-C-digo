97d3eef329a54e3922991d79dec28afbc4bdb094
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-25 20:31:17 -0500

    Add Exercise-6

---
+++ Exercise-6-30-Leap-Years.js
@@ -0,0 +1,27 @@
+/*
+Advertencias:
+- En español por fines didacticos, en la vida real usa nombres en ingles.
+- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
+- Ejercicios genéricos, puedes usar cualquier lenguaje.
+- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
+- Siempre mostrar el resultado en la consola / terminal (salvo excepciones).
+- Hay muchas soluciones validas para un mismo ejercicio.
+ 
+Enunciado Ejercicio 6:
+Dado un año actual, crea un programa que me muestre 
+los años bisiestos que habrá en los proximos 30 años
+ 
+Ejemplo:
+bisiestos(2023);
+ 
+// Salida:
+2024
+2028
+2032
+2036
+2040
+2044
+2048
+2052
+ 
+*/
