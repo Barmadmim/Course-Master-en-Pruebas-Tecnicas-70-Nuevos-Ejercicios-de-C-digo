2ea6d58a187ca9c9fa0522a83cab5d55eb76a1d1
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-28 14:34:36 -0500

    Complete Exercise-1

--- Exercise-1-display-data.js
+++ Exercise-1-display-data.js
@@ -1,4 +1,11 @@
 /*
+Advertencias:
+- En español por fines didacticos, en la vida real usa nombres en ingles.
+- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
+- Ejercicios genéricos, puedes usar cualquier lenguaje.
+- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
+- Siempre mostrar el resultado en la consola / terminal (salvo excepciones).
+- Hay muchas soluciones validas para un mismo ejercicio.
 
 Enunciado Ejercicio 1:
 Dado un array o un objeto de superhéroes de Marvel, haz un programa que pueda
