e99fe7a38824fb14a0e770c100a27042527c2267
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-30 15:52:19 -0500

    Complete Exercise-9

--- Exercise-9-Add-Pairs.js
+++ Exercise-9-Add-Pairs.js
@@ -15,3 +15,21 @@ Ejemplos:
 sumarPares([2, 13, 6, 15]);  // Resultado: 8
  
 */
+
+function sumarPares(numeros) {
+  // Variable para acumular la suma
+  let suma = 0;
+  // Recorrer el array
+  array.forEach((numeroActual) => {
+    // En cada iteracion combrobar si el numero  que estoy  recorriendo es par
+    if (numeroActual % 2 === 0) {
+      // si es par lo sumo
+      suma += numeroActual;
+    }
+  });
+
+  // devolver resultado
+  return suma;
+}
+
+console.log(sumarPares([2, 13, 6, 15]));
