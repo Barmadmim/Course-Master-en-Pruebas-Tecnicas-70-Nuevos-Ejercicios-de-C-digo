eb91f5bf56a5eca947a456fb0cf6af7fe3f364f1
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-30 15:46:48 -0500

    Complete Exercise-8

--- Exercise-8-Whole-To-Roman.js
+++ Exercise-8-Whole-To-Roman.js
@@ -17,9 +17,44 @@ enteroARomano(345)); // Resultado: CCCXLV
  
 */
 
-funtion enteroARomano(numero){
+function enteroARomano(numero) {
+  // Variable para guardar string final
+  let resultado = "";
 
- 
+  // Listado a numeros romanos
+  const caracteresRomanos = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+
+  // Equivalente  en numeros decimales
+  const valoresDecimales = [
+    1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1,
+  ];
+
+  // Bucle para recorrer numeros  decimales
+  for (let i = 0; i < valoresDecimales.length; i++)
+    // Bucle para iterar mientras el numero sea mayor o igual l decimal
+    while (numero >= valoresDecimales[i]) {
+      // Añadir el caracter romano al resultado
+      resultado += caracteresRomanos[i];
+      // Actualizar numero para restar el valor decimal que ya he agregado de numero romanos
+      numero -= valoresDecimales[i];
+    }
+
+  // Devolver resultado
+  return resultado;
 }
 
 console.log(enteroARomano(100));
